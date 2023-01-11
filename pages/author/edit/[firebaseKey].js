eb363b9e6ca9d8e538e82149f70fa5c0dc4391c0
Eric Frey <ericlfrey@gmail.com>
2023-01-11 16:09:02 -0600

    Edit Functionality works

--- pages/author/edit/[firebaseKey].js
+++ pages/author/edit/[firebaseKey].js
@@ -1,13 +1,17 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSingleAuthor } from '../../../api/authorData';
+import AuthorForm from '../../../components/forms/AuthorForm';
 
 export default function EditAuthor() {
+  const [editItem, setEditItem] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleAuthor(firebaseKey).then(setEditItem);
+  }, [firebaseKey]);
   return (
-    <>
-      <div>EditAuthor</div>
-      <div>{firebaseKey}</div>
-    </>
+    <AuthorForm obj={editItem} />
   );
 }
